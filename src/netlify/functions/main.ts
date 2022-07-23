0baa52b0bae7cf503828483ce2e70d58d015e6ce
Alex Harrison <alexjharrison@gmail.com>
2022-07-23 14:54:58 -0400

    remove vite

--- src/netlify/functions/main.ts
+++ src/netlify/functions/main.ts
@@ -1,9 +1,10 @@
 import { Handler } from "@netlify/functions";
+import { text } from "../../payload";
 
 const handler: Handler = async (_event, _context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: text }),
   };
 };
 
