4afc7a86d2e64faa94c2ae5986245afdf6de01e5
Alex Harrison <alexjharrison@gmail.com>
2023-02-08 19:24:08 -0500

    make capitalize safer

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,7 +1,7 @@
 export function capitalize(sentence: string): string {
   return sentence
-    .trim()
     .split(' ')
     .map(word => word[0]?.toUpperCase() + word.slice(1))
+    .filter(word => word !== undefined)
     .join(' ');
 }
